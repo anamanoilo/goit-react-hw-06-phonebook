3a6bc9de2d20b34d4a663bf656304bb68398ed50
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-05-02 18:41:19 +0200

    fix the selector

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -2,8 +2,8 @@ const getFilter = state => state.phonebook.filter;
 const getContacts = state => state.phonebook.contacts;
 
 const getVisibleContacts = state => {
-  const normalizedFilter = getFilter().toLowerCase();
-  return getContacts().filter(contact =>
+  const normalizedFilter = getFilter(state).toLowerCase();
+  return getContacts(state).filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
   );
 };
